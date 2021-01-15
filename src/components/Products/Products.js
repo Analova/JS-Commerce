f81e6e31f4b90d31f45904276ceab2fad3248805
Ana Nikolova <ani.nik19@gmail.com>
2021-01-15 17:02:24 +0100

    fetched the products form commerce.js

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,9 @@
 import React from "react";
 import {Grid} from "@material-ui/core"
 import Product from "./Product/Product"
-
 import useStyles from "./styles"
-const products=[
-    {id:1,name:"Shoes",description:"Running shoes", price:"68", image:"https://ae01.alicdn.com/kf/HTB1VuKkHFXXXXaraXXXq6xXFXXXD/Hot-2018-Spring-Autumn-Lace-Up-Men-s-Canvas-Shoes-Big-Size-Man-Buckle-Casual-Ankle.jpg"},
-    {id:2,name:"MacBook",description:"Apple macbook", price:"2300", image:"https://images.idgesg.net/images/article/2018/11/macbook-air-2108-hero2-100779122-orig.jpeg"},
-]
 
-
-const Products=()=> {
+const Products=({products})=> {
   const classes = useStyles();
   return (
     <main className={classes.content}>
