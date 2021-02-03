3c12ae5c2fbbcc9dba0cc418911387b04f8dafac
Ana Nikolova <ani.nik19@gmail.com>
2021-02-03 15:02:53 +0100

    Checkout component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -30,7 +30,7 @@ const Cart = ({cart ,onUpdateCartQty, onRemoveFromCart, onEmptyCart}) => {
              <div>
                   <div>
                       <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={onEmptyCart} >Empty cart</Button>
-                      <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+                      <Button component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
         </div>
              </div>
        
